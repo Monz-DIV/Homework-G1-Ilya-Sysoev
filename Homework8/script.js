fa31d9516b829b0b59127222953d0e6f9b4a93c6
Monz-DIV <sysoev-div@yandex.ru>
2022-11-30 14:27:13 +0300

    add homework9

--- Homework8/script.js
+++ Homework8/script.js
@@ -1,63 +1,10 @@
-let rise={
-    weight: 1000,
-    price: 200,
-    calories: 1500,
+let pizza = document.getElementById("pizza");
+let pozy = document.getElementById("Pozy");
+let plov = document.getElementById("Plov");
+let tea = document.getElementById("Tea");
+let desert = document.getElementById("Desert");
+
+function printItog() {
+    summa=parseInt(pizza.value) + parseInt(pozy.value) + parseInt(plov.value) + parseInt(tea.value) + parseInt(desert.value);
+    document.getElementById("Itog").value=summa;
 }
-let mutton={
-    weight: 300,
-    price: 700,
-    calories: 2900,
-}
-
-let meat={
-    weight: 500,
-    price: 550,
-    calories: 2900,
-}
-
-let vegetables={
-    weight: 1500,
-    price: 300,
-    calories: 2900,
-}
-
-let dough={
-    weight: 300,
-    price: 200,
-    calories: 2000,
-}
-
-let onion={
-    weight: 400,
-    price: 90,
-    calories: 200,
-}
-
-
-
-let plov=['rise', 'meat', 'vegetables'];
-let pozy=['dough','mutton', 'meat', 'onion'];
-let pizza=['dough','meat', 'onion','vegetables' ];
-
-let food=['plov', 'pozy', 'pizza'];
-
-food.splice (1, 1); // Удалили из массива обьект позы, со смещением позиции пица на место позы.
-
-pozy.splice (1, 1); // Удалили из состава поз баранину.
-
-
-
-food.unshift ('pozy');
-
-
-let pricePlov=(rise.price+meat.price+vegetables.price);
-let pricePozy=(dough.price+mutton.price+meat.price+onion.price);
-let pricePizza=(dough.price+meat.price+onion.price+vegetables.price);
-
-let foodPrice=(pricePlov+pricePozy+pricePizza);
-
-n=food.length; // Вводим переменную n- значение которой заввисит от количества блюд.
-food.splice(1,n); // Удаляем все блюда из меню кроме первого.
-
-
-alert(food);
